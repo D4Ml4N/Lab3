af539c2490f43008b2f2a5e29afe9c802d85cc18
D4Ml4N <galkowski.damian@gmail.com>
2021-05-10 23:25:27 +0200

    Lab3/Zad4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class Controller {
+    public Button btnMain;
+    int clicCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Kliknięcie nr " + ++clicCounter);
+        if (clicCounter == 1)
+            btnMain.setText("I jeszcze raz");
+    }
 }
