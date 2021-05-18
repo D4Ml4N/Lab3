60e0b1eca6c95a82d236ca50a0d2e919d9674155
D4Ml4N <galkowski.damian@gmail.com>
2021-05-18 17:33:19 +0200

    Lab3/Zad6

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,87 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+
+import javafx.scene.control.*;
+import java.text.DecimalFormat;
+import java.util.Optional;
 
 public class Controller {
-    public Button btnMain;
-    public TextField txtLicznik;
-    int clickCounter;
 
-    public void onBtnAction(ActionEvent actionEvent) {
-        txtLicznik.setText("Licznik: " + ++clickCounter);
+    public TextField tfPromien;
+    public TextField tfWysokosc;
+    public TextField tfGrubosc;
+    public Button bWylicz;
+    public Label lNormalnejWaga;
+    public Label lNormalnejKoszt;
+    public Label lNierdzewnejWaga;
+    public Label lNierdzewnejKoszt;
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public void wylicz(ActionEvent actionEvent) {
+        if (!isNumeric(tfPromien.getText()))
+        {
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setHeaderText("Nieprawidłowa wartość promienia walca");
+            Optional<String> result = dialog.showAndWait();
+            return;
+        }
+        if (!isNumeric(tfWysokosc.getText()))
+        {
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setHeaderText("Nieprawidłowa wartość wysokości walca");
+            Optional<String> result = dialog.showAndWait();
+            return;
+        }
+        if (!isNumeric(tfGrubosc.getText()))
+        {
+            TextInputDialog dialog = new TextInputDialog();
+            dialog.setHeaderText("Nieprawidłowa wartość grubosci walca");
+            Optional<String> result = dialog.showAndWait();
+            return;
+        }
+
+
+
+        double normalnejGestosc = 7800;
+        double nierdzewnejGestosc = 7900;
+        double normalnejKoszt = 3.10;
+        double nierdzewnejKoszt = 6.20;
+
+
+
+        float promien =  Float.parseFloat(tfPromien.getText());
+        float wysokosc =  Float.parseFloat(tfWysokosc.getText());
+        float grubosc =  Float.parseFloat(tfGrubosc.getText());
+
+
+        double totalArea = 2 * Math.PI * Math.pow(promien, 2) + 2 * Math.PI * promien * wysokosc;
+        double bordersLength = Math.sqrt(totalArea);
+        double area = Math.pow(bordersLength, 2) * grubosc;
+
+        double normalnejWaga = area * normalnejGestosc;
+        double nierdzewnejWaga = area * nierdzewnejGestosc;
+
+
+
+        DecimalFormat df = new DecimalFormat("###.##");
+        lNormalnejWaga.setText(df.format(normalnejWaga / 1000));
+        lNierdzewnejWaga.setText(df.format(nierdzewnejWaga / 1000));
+        lNormalnejKoszt.setText(df.format(normalnejWaga * normalnejKoszt));
+        lNierdzewnejKoszt.setText(df.format(nierdzewnejWaga * nierdzewnejKoszt));
     }
 }
+
+
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,17 +7,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Kalkulator do blachy");
         primaryStage.setScene(new Scene(root, 300, 275));
+        // primaryStage.setScene(new Scene(root, 600, 440));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
